9b2086da47afb83966294886e9916704a5f8c5f7
Akbarsyadammm <muhamadakbarsyadam@gmail.com>
2023-12-09 13:34:12 +0700

    P01-P12

---
+++ application/controllers/Contoh2.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan Diri</h1>";
+        echo "Nama : Muhamad Akbar Syadam <br>";
+        echo "Nim  : 12220539 <br>";
+        echo "Kelas: 12.3A.05 <br>";
+        echo "TTL  : Bekasi, 19 Mei 2001 <br>";
+        echo "Hobi : Bermain Games Dan  Musik"; 
+        //contoh 1.php//
+    }
+}
